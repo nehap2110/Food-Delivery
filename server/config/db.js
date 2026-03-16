3f599bbc71539f5d1e659f7d9c6a644d9d1b667c
nehap2110 <nehapatel46630@gmail.com>
2026-03-17 00:22:13 +0530

    update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(
-      `${process.env.MONGO_URI}/foodDelivery`
+      `${process.env.MONGO_URI}`
     );
 
     console.log(`✅ Database Connected: ${conn.connection.host}`);
